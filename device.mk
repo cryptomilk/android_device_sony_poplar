6c20f178492827910be2df546238ff11ea168cc4
sjllls <2012ji123@gmail.com>
2019-02-05 20:05:55 +0800

    Upgrade device.mk

--- device.mk
+++ device.mk
@@ -3,10 +3,28 @@ $(call inherit-product, device/sony/yoshino/platform.mk)
 ### PROPRIETARY VENDOR FILES
 $(call inherit-product, vendor/sony/poplar/poplar-vendor.mk)
 
-### DALVIK/HWUI
+ifeq ($(WITH_FDROID),true)
+$(call inherit-product, vendor/fdroid/fdroid-vendor.mk)
+endif
+ifeq ($(WITH_MICROG),true)
+$(call inherit-product, vendor/microg/microg-vendor.mk)
+endif
+
+### DALVIK
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 DEVICE_PATH := device/sony/poplar
 
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay
+
+ifeq ($(WITH_TWRP),true)
+include $(DEVICE_PATH)/device/init.mk
+else # WITH_TWRP
 include $(DEVICE_PATH)/device/*.mk
+include $(DEVICE_PATH)/vendor_prop.mk
+endif # WITH_TWRP
